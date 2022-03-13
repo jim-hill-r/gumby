0a33f9d0ad53c661c92f891837ec428543869210
Jim hill <jim.hill.r@gmail.com>
2022-03-13 12:39:45 -0700

    Plan for triathalong

--- pwa/src/pages/plan.tsx
+++ pwa/src/pages/plan.tsx
@@ -1,85 +1,283 @@
 import React, { useState } from "react"
+import  '../styles/grid.css'
 
+const toPercent = (float:number) => {
+  if (float > 1) {
+    return `${((float - 1) * 100).toFixed(2)}%`
+  } else {
+    return `${((1 - float) * 100).toFixed(2)}%`
+  }
+}
 const getWeek = (date:Date) => {
   return 17
 }
 
-export default function PlanPage () {
-  const [ startWeek, setStartWeek ] = useState<number>(getWeek(new Date(Date.now())))
-  const [ startDistance, setStartDistance ] = useState<number>(1)
-  const [ startPace, setStartPace ] = useState<number>(10)
+const calculateGainRate = (start, target, iterations) => {
+  return ( target / start ) ** ( 1 / iterations)
+}
 
-  const [ targetWeek, setTargetWeek ] = useState<number>(startWeek + 18)
-  const [ targetDistance, setTargetDistance ] = useState<number>(startDistance + 2)
-  const [ targetPace, setTargetPace ] = useState<number>(startPace - 1)
+const getTriathlonPlan = (swimStart, swimTarget, bikeStart, bikeTarget, runStart, runTarget, weeks) => {
+  const swimDistanceRate = calculateGainRate(swimStart.distance,swimTarget.distance,weeks)
+  const swimPaceRate = calculateGainRate(swimStart.pace,swimTarget.pace,weeks)
+  const bikeDistanceRate = calculateGainRate(bikeStart.distance,bikeTarget.distance,weeks)
+  const bikePaceRate = calculateGainRate(bikeStart.pace,bikeTarget.pace,weeks)
+  const runDistanceRate = calculateGainRate(runStart.distance,runTarget.distance,weeks)
+  const runPaceRate = calculateGainRate(runStart.pace,runTarget.pace,weeks)
 
-  const weeks = []
-  const numWeeks = targetWeek - startWeek
-  const gainsRate = (targetDistance/startDistance)**(1/numWeeks)
-  for (let i = 0; i<=numWeeks; i++) {
-    weeks.push({
-      weekNumber: i + startWeek,
-      distance: startDistance * (gainsRate**i),
-      pace: "TODO",
-      percentChange: gainsRate
+  const weeklyPlan = []
+  for (let i=0; i<=weeks; i++) {
+    const swim = {
+      distance: swimStart.distance * (swimDistanceRate ** i),
+      pace: swimStart.pace * (swimPaceRate ** i)
+    }
+    const bike = {
+      distance: bikeStart.distance * (bikeDistanceRate ** i),
+      pace: bikeStart.pace * (bikePaceRate ** i)
+    }
+    const run = {
+      distance: runStart.distance * (runDistanceRate ** i),
+      pace: runStart.pace * (runPaceRate ** i)
+    }
+    const brick = {
+
+    }
+    const strength = {
+
+    }
+    const cross = {
+
+    }
+    const gains = {
+      swim: {
+        distance: swimDistanceRate,
+        pace: swimPaceRate
+      },
+      bike: {
+        distance: bikeDistanceRate,
+        pace: bikePaceRate
+      },
+      run: {
+        distance: runDistanceRate,
+        pace: runPaceRate
+      }
+    }
+    weeklyPlan.push({
+      swim,
+      bike,
+      run,
+      brick,
+      strength,
+      cross,
+      gains
     })
   }
+  return weeklyPlan
+}
+
+export default function PlanPage () {
+  const [ startWeek, setStartWeek ] = useState<number>(getWeek(new Date(Date.now())))
+  const [ startSwimDistance, setStartSwimDistance ] = useState<number>(1)
+  const [ startSwimPace, setStartSwimPace ] = useState<number>(10)
+  const [ startBikeDistance, setStartBikeDistance ] = useState<number>(1)
+  const [ startBikePace, setStartBikePace ] = useState<number>(10)
+  const [ startRunDistance, setStartRunDistance ] = useState<number>(1)
+  const [ startRunPace, setStartRunPace ] = useState<number>(10)
+
+  const [ targetWeek, setTargetWeek ] = useState<number>(startWeek + 18)
+  const [ targetSwimDistance, setTargetSwimDistance ] = useState<number>(1)
+  const [ targetSwimPace, setTargetSwimPace ] = useState<number>(10)
+  const [ targetBikeDistance, setTargetBikeDistance ] = useState<number>(1)
+  const [ targetBikePace, setTargetBikePace ] = useState<number>(10)
+  const [ targetRunDistance, setTargetRunDistance ] = useState<number>(1)
+  const [ targetRunPace, setTargetRunPace ] = useState<number>(10)
+
+  const weeks = targetWeek - startWeek
+  let plan = getTriathlonPlan(
+  {
+    distance: startSwimDistance,
+    pace: startSwimPace
+  },
+  {
+    distance: targetSwimDistance,
+    pace: targetSwimPace
+  },
+  {
+    distance: startBikeDistance,
+    pace: startBikePace
+  },
+  {
+    distance: targetBikeDistance,
+    pace: targetBikePace
+  },
+  {
+    distance: startRunDistance,
+    pace: startRunPace
+  },
+  {
+    distance: targetRunDistance,
+    pace: targetRunPace
+  },
+  weeks
+  )
 
   return (
     <>
-      <h3>Start</h3>
-      <div>
-        <label>Week</label>
-        <input type="number" value={startWeek} onChange={(event) => setStartWeek(parseInt(event.target.value))}></input>
+      <div className="row">
+        <div className="column">
+          <h3>Start</h3>
+          <div>
+            <label>Week</label>
+            <input type="number" value={startWeek} onChange={(event) => setStartWeek(parseInt(event.target.value))}></input>
+          </div>
+        </div>
       </div>
-      <div>
-        <label>Distance</label>
-        <input type="number" value={startDistance} onChange={(event) => setStartDistance(event.target.value)}></input>
-      </div>
-      <div>
-        <label>Pace</label>
-        <input type="number" value={startPace} onChange={(event) => setStartPace(event.target.value)}></input>
-      </div>
+      <div className="row">
+        <div className="column">
+          <h4>Swim</h4>
+          <div>
+            <label>Distance</label>
+            <input type="number" value={startSwimDistance} onChange={(event) => setStartSwimDistance(parseFloat(event.target.value))}></input>
+          </div>
+          <div>
+            <label>Pace</label>
+            <input type="number" value={startSwimPace} onChange={(event) => setStartSwimPace(parseFloat(event.target.value))}></input>
+          </div>
+        </div>
+        <div className="column">
+        <h4>Bike</h4>
+          <div>
+            <label>Distance</label>
+            <input type="number" value={startBikeDistance} onChange={(event) => setStartBikeDistance(parseFloat(event.target.value))}></input>
+          </div>
+          <div>
+            <label>Pace</label>
+            <input type="number" value={startBikePace} onChange={(event) => setStartBikePace(parseFloat(event.target.value))}></input>
+          </div>
+        </div>
+        <div className="column">
+        <h4>Run</h4>
+          <div>
+            <label>Distance</label>
+            <input type="number" value={startRunDistance} onChange={(event) => setStartRunDistance(parseFloat(event.target.value))}></input>
+          </div>
+          <div>
+            <label>Pace</label>
+            <input type="number" value={startRunPace} onChange={(event) => setStartRunPace(parseFloat(event.target.value))}></input>
+          </div>
+        </div>
+      </div> 
       
-      <h3>Target</h3>
-      <div>
-        <label>Week</label>
-        <input type="number" value={targetWeek} onChange={(event) => setTargetWeek(parseInt(event.target.value))}></input>
+      <div className="row">
+        <div className="column">
+          <h3>Target</h3>
+          <div>
+            <label>Week</label>
+            <input type="number" value={targetWeek} onChange={(event) => setTargetWeek(parseInt(event.target.value))}></input>
+          </div>
+        </div>
       </div>
-      <div>
-        <label>Distance</label>
-        <input type="number" value={targetDistance} onChange={(event) => setTargetDistance(event.target.value)}></input>
-      </div>
-      <div>
-        <label>Pace</label>
-        <input type="number" value={targetPace} onChange={(event) => setTargetPace(event.target.value)}></input>
-      </div>
+      <div className="row">
+        <div className="column">
+          <h4>Swim</h4>
+          <div>
+            <label>Distance</label>
+            <input type="number" value={targetSwimDistance} onChange={(event) => setTargetSwimDistance(parseFloat(event.target.value))}></input>
+          </div>
+          <div>
+            <label>Pace</label>
+            <input type="number" value={targetSwimPace} onChange={(event) => setTargetSwimPace(parseFloat(event.target.value))}></input>
+          </div>
+        </div>
+        <div className="column">
+        <h4>Bike</h4>
+          <div>
+            <label>Distance</label>
+            <input type="number" value={targetBikeDistance} onChange={(event) => setTargetBikeDistance(parseFloat(event.target.value))}></input>
+          </div>
+          <div>
+            <label>Pace</label>
+            <input type="number" value={targetBikePace} onChange={(event) => setTargetBikePace(parseFloat(event.target.value))}></input>
+          </div>
+        </div>
+        <div className="column">
+        <h4>Run</h4>
+          <div>
+            <label>Distance</label>
+            <input type="number" value={targetRunDistance} onChange={(event) => setTargetRunDistance(parseFloat(event.target.value))}></input>
+          </div>
+          <div>
+            <label>Pace</label>
+            <input type="number" value={targetRunPace} onChange={(event) => setTargetRunPace(parseFloat(event.target.value))}></input>
+          </div>
+        </div>
+      </div> 
 
       <h3>Plan</h3>
       <table>
         <thead>
           <tr>
             <th>Week</th>
-            <th>Distance</th>
-            <th>Pace</th>
-            <th>Gains</th>
+            <th colSpan={4}>Swim</th>
+            <th colSpan={4}>Bike</th>
+            <th colSpan={4}>Run</th>
+            <th colSpan={4}>Bonus</th>
+          </tr>
+          <tr>
+            <th></th>
+            <th colSpan={2}>Distance</th>
+            <th colSpan={2}>Pace</th>
+            <th colSpan={2}>Distance</th>
+            <th colSpan={2}>Pace</th>
+            <th colSpan={2}>Distance</th>
+            <th colSpan={2}>Pace</th>
+            <th colSpan={2}>Distance</th>
+            <th colSpan={2}>Pace</th>
           </tr>
         </thead>
         <tbody>
-        {weeks.map((w,idx) => {
+        {plan.map((w,idx) => {
           return <tr key={idx}>
-            <th>
-              {w.weekNumber}
-            </th>
-            <th>
-              {w.distance.toFixed(2)}
-            </th>
-            <th>
-              {w.pace}
-            </th>
-            <th>
-              {((w.percentChange - 1)*100).toFixed(2)} %
-            </th>
+            <td>
+              {idx + startWeek}
+            </td>
+            <td>
+              {w.swim.distance.toFixed(2)}
+            </td>
+            <td>
+              {toPercent(w.gains.swim.distance)}
+            </td>
+            <td>
+              {w.swim.pace.toFixed(2)}
+            </td>
+            <td>
+              {toPercent(w.gains.swim.pace)}
+            </td>
+            <td>
+              {w.bike.distance.toFixed(2)}
+            </td>
+            <td>
+              {toPercent(w.gains.bike.distance)}
+            </td>
+            <td>
+              {w.bike.pace.toFixed(2)}
+            </td>
+            <td>
+              {toPercent(w.gains.bike.pace)}
+            </td>
+            <td>
+              {w.run.distance.toFixed(2)}
+            </td>
+            <td>
+              {toPercent(w.gains.run.distance)}
+            </td>
+            <td>
+              {w.run.pace.toFixed(2)}
+            </td>
+            <td>
+              {toPercent(w.gains.run.pace)}
+            </td>
+            <td>
+            </td>
           </tr>
         })}
         </tbody>
